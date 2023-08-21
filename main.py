f5a03578aab9eb392be9bb713a20ffcd93e7652f
Jona Walpert <98217482+Jona-Walpert@users.noreply.github.com>
2023-08-21 21:36:37 +0200

    Add files via upload
    print("under construction, please wait....")

---
+++ main.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import requests
+import csv
+from requests.api import head
+import pygame
+
+
+
+
+pygame.init()
+SCREEN_WIDTH = 1920 
+SCREEN_HEIGHT = 1080
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+time = ["22","23","0","1","2","3"]
+
+
+text_font = pygame.font.SysFont("Arial", 35, bold=True)
+
+
+def drawscreen(word):
+            screen.fill((0,0,0))
+
+            draw_text(word, text_font, (255,255,255), 800, 500)
+
+            pygame.display.flip()
+
+def draw_text(text, font, text_col, x, y):
+          img = font.render(text, True, text_col)
+          screen.blit(img, (x, y))
+
+
+run = True
+while run:
+
+
+
+
+
+    data = requests.get('your csv api key').text
+
+
+           
+    tcc_arr = []
+
+    for tcc_data in range(22,28):
+          data_text = str(data).splitlines()[tcc_data].split(',')
+          tcc = int(data_text[6])
+          tcc_arr.append(tcc)
+
+
+
+    for x in range(0,6):
+        print("Uhrzeit: ", time[x]," Uhr" " : ", "Gesamt Wolkenbedeckung= ", tcc_arr[x], "%")
+
+
+    
+    drawscreen()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+    time.sleep(1000000000)
+        
+    
