8b20d90ed64b4aba6bba0455a7e0d5c20d290811
Jona Walpert <98217482+Jona-Walpert@users.noreply.github.com>
2023-12-03 16:54:34 +0100

    yet to be implemented: color text background dark when its dark outside

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import threading
 import ctypes
 user32 = ctypes.windll.user32
 
-api_key = "YOURAPIKEY"
-logoname ="logoname.png (PICTURE IN SAME DIRECTORY AS main.py)" #255 x 255 pixels
+api_key = "zfBA7Y4U5RrusykZ"
+logoname ="meteobluelogo.png" #255 x 255 pixels
 
 pygame.init()
 SCREEN_WIDTH = user32.GetSystemMetrics(0)
@@ -19,14 +19,35 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('API weather display')
 
 sideoffset = (SCREEN_WIDTH - 1160) / 3
+tcclevel = 0
+daylightlevel = 0
 
 textsize = 20
 text_font = pygame.font.SysFont("Arial", textsize, bold=True)
 
 
-def drawscreen(word,x,offset):
+def drawscreen(word,x,offset): 
+    # daylight = []
+    # for y in range (1,49):
+    #     z = isdaylight(y)  
+    #     if z == 1 or z == 0:
+    #         daylight.append(z)
+    #         print("appended")
+    #     print(z) 
+    #     if daylight[y] == 1:
+    #         color = (16, 14, 43)
+    #         pygame.draw.rect(screen, color, pygame.Rect(x, (offset+1)*20, 550, 30))
+        
+    #     if daylight[y] != 1:
+    #         print(z)
+            
+    #     if daylight[y] >= 2:
+    #         print("FELHER")
+        
 
     draw_text(word, text_font, (255,255,255), x, (offset+1)*20)
+
+    
 
     pygame.display.flip()
     
@@ -48,6 +69,11 @@ def gettcc(x):
     tcc = tcc_text[6] if tcc_text[6] else '0'
     return tcc
 
+def isdaylight(x):
+    daylight_text = str(data).splitlines()[x].split(',')
+    daylight = daylight_text[19] if daylight_text[19] else '0'
+    return int(daylight)
+
 
 data = requests.get(f'https://my.meteoblue.com/packages/basic-1h_clouds-1h?apikey={api_key}&lat=47.615&lon=7.66457&asl=308&format=csv').text
 
@@ -58,13 +84,18 @@ imp = pygame.image.load(logoname).convert()
 
 def main():
     clearscreen()
-    for x in range(1,24):
-        result_text = getdate(x) + " Uhr" + " : " + "Gesamt Wolkenbedeckung= " + gettcc(x) + " %"
-        drawscreen(result_text, sideoffset-50, x*1.6)
-    for x in range(25,48):
-        result_text = getdate(x) + " Uhr" + " : " + "Gesamt Wolkenbedeckung= " + gettcc(x) + " %"
-        drawscreen(result_text,2* sideoffset + 580+50, (x-24)*1.6)
-    screen.blit(imp, ((sideoffset+100-255)/2 + sideoffset-50 + 580, SCREEN_HEIGHT/2 - 255/2))
+        
+    for x in range (1,49):
+        print(x)
+        #z = isdaylight(x)
+        if x < 25:
+            result_text = getdate(x) + " Uhr" + " : " + "Gesamt Wolkenbedeckung= " + gettcc(x) + " %"
+            drawscreen(result_text, sideoffset-50, (x*1.6)-0.5)
+        else:
+            result_text = getdate(x) + " Uhr" + " : " + "Gesamt Wolkenbedeckung= " + gettcc(x) + " %"
+            drawscreen(result_text,2* sideoffset + 580+50, ((x-24)*1.6)-0.5)
+        
+    screen.blit(imp, ((sideoffset+100-255)/2 + sideoffset-50 + 550, SCREEN_HEIGHT/2 - 255/2))
     pygame.display.flip()
 
 
